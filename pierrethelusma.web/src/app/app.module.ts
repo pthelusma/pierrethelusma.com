b004610e256f181d390fece89815970d2bc42df1
Pierre Thelusma <pthelusma@gmail.com>
2017-09-06 23:38:22 -0400

    Creating a few components for site

--- pierrethelusma.web/src/app/app.module.ts
+++ pierrethelusma.web/src/app/app.module.ts
@@ -2,17 +2,44 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
+// ROOT
 import { AppComponent } from './app.component';
+
+// ROUTES
+import { HomeComponent } from './home/home.component';
+
+// COMPONENTS
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
+import { AboutComponent } from './about/about.component';
+import { PageNotFoundComponent } from './pagenotfound/pagenotfound.component';
+import { UnauthorizedComponent } from './unauthorized/unauthorized.component';
+
+const appRoutes: Routes = [
+  { path: 'about', component: AboutComponent },
+  { path: '', component: HomeComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    ContentComponent,
+    AboutComponent,
+    HomeComponent,
+    PageNotFoundComponent,
+    UnauthorizedComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
